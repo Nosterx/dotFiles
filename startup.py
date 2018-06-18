bd631f920744aa02f16d38413a6b380d2b4a76a6
Nosterx <nosterx@gmail.com>
2018-06-18 22:48:45 +0500

    add run commands into ipython

--- startup.py
+++ startup.py
@@ -1,26 +1,42 @@
 from IPython.core.magic import register_line_magic
+from subprocess import call
+import os
+
+
+os.environ['CONSUL_HTTP_ADDR'] = "consul.dev.cian.ru:8500"
 
 
 @register_line_magic
 def ecsl(self):
-    import os
     os.environ['SIMPLE_SETTINGS'] = 'countryside.settings.local'
     return "export ['SIMPLE_SETTINGS'] = countryside.settings.local"
 
 @register_line_magic
 def ets(self):
-    import os
     os.environ['SIMPLE_SETTINGS'] = 'tests.settings'
     return "export ['SIMPLE_SETTINGS'] = tests.settings"
 
 @register_line_magic
 def ecssl(self):
-    import os
     os.environ['SIMPLE_SETTINGS'] = 'countryside_search.settings.local'
     return "export ['SIMPLE_SETTINGS'] = countryside_search.settings.local"
 
 @register_line_magic
-def ec(self):
-    import os
-    os.environ['CONSUL_HTTP_ADDR'] = "consul.dev.cian.ru:8500"
-    return "export ['CONSUL_HTTP_ADDR'] = consul.dev.cian.ru:8500"
+def runc(self):
+    print(ecsl(self))
+    call(['countryside', 'serve'])
+    
+@register_line_magic
+def runcs(self):
+    print(ecssl(self))
+    call(['countryside-serve', 'serve'])
+    
+@register_line_magic
+def rtest(self):
+    print(ets(self))
+    call('pytest')
+
+@register_line_magic
+def rtox(self):
+    print(ets(self))
+    call('tox')
